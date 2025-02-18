5c33b06308083d5070b1e17aff6470d88a90981e
HAHAHAHAHHA21 <alexbox365@live.com>
2025-02-18 01:15:42 -0500

    Removed some unnecessary type annotations

--- barycentric.py
+++ barycentric.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-def get_barycentric_coordinates(triangle_coordinates:np.ndarray[np.float64], point_coordinates:np.ndarray[np.float64]) -> np.ndarray:
+def get_barycentric_coordinates(triangle_coordinates:np.ndarray, point_coordinates:np.ndarray) -> np.ndarray:
     """Returns the barycentric coordinates of a cartesian point (x,y) with
     respect to a triangle with vertices (x1,y1), (x2,y2), (x3,y3)"""
     barycentric_point = np.linalg.solve(triangle_coordinates, point_coordinates)
     return np.asarray(barycentric_point)
 
-def get_cartesian_coordinates(triangle_coordinates:np.ndarray[np.float64], barycentric_coordinates:np.ndarray[np.float64]) -> np.ndarray[np.float64]:
+def get_cartesian_coordinates(triangle_coordinates:np.ndarray, barycentric_coordinates:np.ndarray) -> np.ndarray:
     """Returns the cartesian coordinates (x,y) of a point in barycentric coordinates
     with respect to a triangle with vertices (x1,y1), (x2,y2), (x3,y3)"""
     fixed_vector = np.transpose(barycentric_coordinates) #changes to appropriate dimensions
     cartesian_point = np.matmul(triangle_coordinates, fixed_vector) #matrix-vector mult
     return cartesian_point
 
-def is_inside_triangle(triangle_coordinates:np.ndarray[np.float64], point_coordinates:np.ndarray[np.float64]) -> bool:
+def is_inside_triangle(triangle_coordinates:np.ndarray, point_coordinates:np.ndarray) -> bool:
     """Determines whether a point (x,y) is inside a triangles with vertices
      (x1,y1), (x2,y2), (x3,y3)"""
     barycentric_point = get_barycentric_coordinates(triangle_coordinates, point_coordinates)
