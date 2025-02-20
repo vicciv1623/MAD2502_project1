4b5dad09ee69620cf5d9c56f9d9b84d89b0bfb19
vicciv1623 <143960937+vicciv1623@users.noreply.github.com>
2025-02-19 23:35:43 -0500

    Update riemann.py to include simpson function

--- riemann.py
+++ riemann.py
@@ -9,3 +9,9 @@ def trapezoid(x_vals, func):
     widths = np.diff(x_vals)
     heights = (func(x_vals[:-1]) + func(x_vals[1:])) / 2
     return np.sum(widths * heights)
+
+def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
+  a_vals = x_vals[:-1]
+  b_vals = x_vals[1:]
+  fun_mid = func((a_vals + b_vals)/2)
+  return np.sum((b_vals - a_vals)/6 * (func(a_vals) + 4*fun_mid + func(b_vals)))
