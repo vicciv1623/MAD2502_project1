f4167e1f7b80ef6429203a2ab49f6dacb00206bc
vicciv1623 <143960937+vicciv1623@users.noreply.github.com>
2025-02-19 23:39:52 -0500

    Delete simpson.py

--- simpson.py
+++
@@ -1,5 +0,0 @@
-def simpson(x_vals: np.ndarray, func: np.ufunc) -> float:
-  a_vals = x_vals[:-1]
-  b_vals = x_vals[1:]
-  fun_mid = func((a_vals + b_vals)/2)
-  return np.sum((b_vals - a_vals)/6 * (func(a_vals) + 4*fun_mid + func(b_vals)))
